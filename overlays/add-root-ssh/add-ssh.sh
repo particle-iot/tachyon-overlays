811db872d9eb301d3103238eb9aa3a09b981ac31
Nick Lambourne <github@lamb-chop.co.uk>
2025-12-18 10:11:11 -0800

    Open up SSH for login

--- overlays/add-root-ssh/add-ssh.sh
+++ overlays/add-root-ssh/add-ssh.sh
@@ -10,6 +10,16 @@ echo "Modifying /etc/ssh/sshd_config..."
 sed -i 's/^#Port 22/Port 22/' /etc/ssh/sshd_config
 sed -i 's/^ListenAddress.*/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
 
+# Enable root login (uncomment and change existing line)
+sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+# Enable password authentication (add if not present, otherwise replace)
+if grep -q "^#*PasswordAuthentication" /etc/ssh/sshd_config; then
+    sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+else
+    echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+fi
+
 echo "Removing existing host keys, they will be generated uniquely on boot..."
 rm -f /etc/ssh/ssh_host_*
 
